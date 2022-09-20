aec0c69ef1415acf6cfc95ce1855bb26184ce7a6
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2022-09-20 16:11:06 -0400

    Working on Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
 
@@ -13,10 +14,35 @@ export default function Hero({}: Props) {
   return (
     <div className='flex flex-col h-screen space-y-8 items-center justify-center text-center overflow-hidden'>
       <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor='#f7ab0a' />
-      </h1>
+      <img
+        className='relative rounded-full h-32 w-32 mx-auto object-cover'
+        src='/images/mfdoom.jpg'
+        alt='MF Doom'
+      />
+      <div>
+        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>
+          Front End Developer
+        </h2>
+        <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+          <span className='mr-3'>{text}</span>
+          <Cursor cursorColor='#f7ab0a' />
+        </h1>
+
+        <div className='pt-5'>
+          <Link href='#about'>
+            <button className='heroButton'>About</button>
+          </Link>
+          <Link href='#experience'>
+            <button className='heroButton'>Experience</button>
+          </Link>
+          <Link href='#skills'>
+            <button className='heroButton'>Skills</button>
+          </Link>
+          <Link href='#projects'>
+            <button className='heroButton'>Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
