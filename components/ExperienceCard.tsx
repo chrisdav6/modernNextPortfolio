4f10c3f6c4f46bc7e1b4c1962d600ab53b8cc5cc
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2022-09-22 10:23:17 -0400

    Completed Experience component

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 export default function ExperienceCard({}: Props) {
   return (
-    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10'>
+    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 opacity-40 hover:opacity-100 cursor-pointer transition-opacity duration-200 overflow-hidden'>
       <motion.img
         initial={{ y: -100, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
